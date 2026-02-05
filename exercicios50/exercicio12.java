ee3374ecb1015b6f6ba7bfb9f659d521363cc1c4
MauricioZagato <mazaol777@gmail.com>
2026-02-04 22:14:18 -0300

    mais um commit

---
+++ exercicios50/exercicio12.java
@@ -0,0 +1,14 @@
+public class exercicio12 {
+   static void imprimirQuadrado(int n, int n2){
+    double resultado;
+    n = 12;
+
+    resultado = (n = (n*n));
+
+    System.out.println("O quadrado de n é n², então a resposta é: " + resultado);
+    }
+
+    public static void main(String[] args) {
+        imprimirQuadrado(0, 0);
+    }
+}
---
+++ exercicios50/exercicio13.java
@@ -0,0 +1,13 @@
+public class exercicio13 {
+    static void imprimirSoma(int a, int b, int soma){
+        a = 3;
+        b = 7;
+
+        soma = a + b;
+        System.out.println("A soma é: " + soma);
+    }
+
+    public static void main(String[] args) {
+        imprimirSoma(0, 0, 0);
+    }
+}
